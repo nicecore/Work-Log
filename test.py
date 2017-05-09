5774fab0299632aa5d205fa9f2719d7cafb55f26
Adam Cameron <adamdcameron@gmail.com>
2017-05-09 10:40:36 -0300

    new_entry function working

--- test.py
+++ test.py
@@ -1,3 +1,19 @@
-print(
-"""Hey dude!\n> 
+notnumber = True
+while notnumber:
+    time_spent = input(
+"""***NEW ENTRY***
+
+Please enter the amount of time spent on the task, in minutes:\n> 
 """)
+    try:
+        time_spent = int(time_spent)
+    except ValueError:
+        # clear_screen()
+        input("You need to enter a number! Please press ENTER to try again...")
+        time_spent = input(
+"""***NEW ENTRY***
+
+Please enter the amount of time spent on the task, in minutes
+""")
+    else:
+        notnumber = False
--- worklog.py
+++ worklog.py
@@ -18,59 +18,106 @@ def clear_screen():
 ##########################################################################
 
 def new_entry():
+
     clear_screen()
-    # Prompt user for task name
-    task_name = input(
-"""Please enter the name of the task performed:\n> 
-""")
-    
-    # Prompt user for time spent on {}.format(task_name)
-    time_spent = int(input(
-"""Please enter the amount of time spent on the task, in minutes:\n> 
-"""))
+    still_entering = True
+    while still_entering:
+        
 
-    # Prompt user to input notes about task
+        # Prompt user for task name
+        task_name = input(
+"""***NEW ENTRY***
 
-    notes = input(
-"""Please enter any relevant notes about this task:\n> 
+Please enter the name of the task performed:\n> 
 """)
+        # Prompt user for time spent on {}.format(task_name)
+        clear_screen()
+        notnumber = True
+        # While block with try/except to make sure user enters an integer
+        while notnumber:
+            time_spent = input(
+"""***NEW ENTRY***
+
+Please enter the amount of time spent on task "{}", in minutes:\n> 
+""".format(task_name))
+            try:
+                time_spent = int(time_spent)
+            except ValueError:
+                clear_screen()
+                input("You need to enter a number! Please press ENTER to try again...")
+                time_spent = input(
+"""***NEW ENTRY***
+
+Please enter the amount of time spent on task "{}", in minutes
+""".format(task_name))
+            else:
+                notnumber = False
+        # Prompt user to input notes about task
+        clear_screen()
+        notes = input(
+"""***NEW ENTRY***
+
+Please enter any relevant notes about task "{}":\n> 
+""".format(task_name))
+
+        with open("worklog.csv", "a", newline="") as file:
+            filewriter = csv.writer(file)
+            filewriter.writerow([task_name, time_spent, notes])
+
+        clear_screen()
+        unchosen = True
+        while unchosen:
+            another_entry = input(
+"""Would you like to make another entry in the work log?
+Please enter Y for YES or N for NO.
+""").lower()
+            if another_entry == 'y' or another_entry == 'yes':
+                unchosen = False
+                clear_screen()
+            elif another_entry == 'n' or another_entry == 'no':
+                unchosen = False
+                still_entering = False
+                clear_screen()
 
     # Write these to a line on the CSV file separated by commas
     # Upon successfully writing inputs to a line of CSV file,
     # ask if user wants to make another entry
     # If not, return user to initial prompt
 
-    pass
 
 
 ##########################################################################
 
 def search_regex():
-    
-    reg_search = input(
+    with open("worklog.csv") as file:
+        file_reader = csv.reader(file)
+        reg_search = input(
 """Please enter a regular expression to be searched:\n> 
 """)
-    # Prompt user for a regular expression
-    # Search CSV file for a line containing regular expression
-    # Print line out to user, press ENTER to continue
-    # Ask if user wants to do another regex search
-    # If not, return user to initial prompt
-    pass
+        # Prompt user for a regular expression
+        # Search CSV file for a line containing regular expression
+        # Print line out to user, press ENTER to continue
+        # Ask if user wants to do another regex search
+        # If not, return user to initial prompt
+        pass
 
 
 ##########################################################################
 
 
 def search_plain():
-    plain_search = input(
+    with open("worklog.csv") as file:
+        file_reader = csv.reader(file)
+
+        plain_search = input(
 """Please enter a word or phrase to be searched
 """)
-    # Prompt user for a string to search from
-    # Search CSV file for a line containing the string
-    # Print out entire line to user, press ENTER to continue
-    # Ask if user wants to do another regex search
-    # IF not, return user to initial prompt
-    pass
+        # Prompt user for a string to search from
+        # Search CSV file for a line containing the string
+        # Print out entire line to user, press ENTER to continue
+        # Ask if user wants to do another regex search
+        # IF not, return user to initial prompt
+        pass
 
 
 ##########################################################################
@@ -81,7 +128,7 @@ def search_plain():
 
 def main():
     clear_screen()
-    print("""
+    choice = input("""
 Welcome, wage slave!
 
 Please type one of the options below and hit ENTER.
@@ -94,5 +141,9 @@ Please type one of the options below and hit ENTER.
 To cancel a new entry or a search, enter BACK
 at any point during your session to return to this prompt.
 
-""")
+""").lower()
+    if choice == 'a':
+        new_entry()
+
+main()
 
