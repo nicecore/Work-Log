5e5daf5c1734433875440315bdd49fb0adbbb090
Adam Cameron <adamdcameron@gmail.com>
2017-05-15 15:47:38 -0300

    major refactor, reader and writer functions defined and implemented into main search functions

--- worklog.py
+++ worklog.py
@@ -6,12 +6,44 @@ import re
 # Work Log, a Treehouse Tech Degree Project
 # by Adam Cameron, May 2017
 
-def printer():
-    
 
-    pass
+def reader():
+    """Return updated list of CSV file's contents"""
+    with open("worklog.csv", "r") as readfile:
+        reader = csv.reader(readfile)
+        next(reader)
+        contents = []
+        for row in reader:
+            contents.append(row)
+        return contents
+
+
+def printer(results, search):
+    """Print out results of a search"""
+    c_s()
+    print("titles for {}".format(search)) # Format 'search' here to show what the user searched
+    for i in results:
+        print(', '.join(i))
+
+def writer(date, time, task, notes):
+    """Write user input to CSV file as a new row"""
+    with open("worklog.csv", "a", newline="") as file:
+        writer = csv.writer(file, delimiter=",")
+        writer.writerow([date, time, task, notes])
+
+def search_again():
+    search_again = input(
+"""\nWould you like to do another search?
+
+Please press ENTER if YES, and N and ENTER if NO:
+> """).lower()
+    if search_again == 'n' or search_again == 'no':
+        return False
+    else:
+        return True
 
 def verify_num():
+    """Verify that the user entered a number"""
     while True:
         c_s()
         try:
@@ -24,18 +56,6 @@ Press ENTER to try again...""")
         else:
             return str(a)
             break
-
-def date_verifier(prompt):
-
-    pass
-
-def verifier(prompt):
-
-    pass
-
-with open("worklog.csv", "a", newline="") as file:
-    filewriter = csv.writer(file)
-
 
 def c_s():
     """Clear screen function"""
@@ -51,7 +71,7 @@ def new_entry():
         while enter_task:
             c_s()
             task_name = input(
-                """***NEW ENTRY***
+"""***NEW ENTRY***
 
 Please enter the name of the task performed:\n> """)
             if task_name:
@@ -61,24 +81,8 @@ Please enter the name of the task performed:\n> """)
                 input("""
 Please enter a title for this task! Press ENTER to try again...
 """)
-        # Prompt user for time spent on {}.format(task_name)
         c_s()
-        notnumber = True
-        while notnumber:
-            time_spent = input(
-                """***NEW ENTRY***
-
-Please enter the number of minutes spent on task "{}":
-> """.format(task_name))
-            try:
-                time_spent = int(time_spent)
-            except ValueError:
-                c_s()
-                input("You need to enter a number! Please press ENTER to try again...")
-                c_s()
-            else:
-                notnumber = False
-        # Prompt user to input notes about task
+        time_spent = verify_num()
         c_s()
         notes = input(
             """***NEW ENTRY***
@@ -87,12 +91,10 @@ Please enter any relevant notes about task "{}":
 > """.format(task_name))
         entry_datetime = datetime.datetime.now()
         date_fmt = entry_datetime.strftime('%m/%d/%Y @ %-I:%M %p')
-        with open("worklog.csv", "a", newline="") as file:
-            filewriter = csv.writer(file)
-            filewriter.writerow([date_fmt, task_name, time_spent, notes])
-        c_s()
+        writer(date_fmt, task_name, time_spent, notes)
         unchosen = True
         while unchosen:
+            c_s()
             another_entry = input(
                 """Would you like to make another entry in the work log?
 Please enter Y for YES or N for NO.
@@ -108,54 +110,35 @@ Please enter Y for YES or N for NO.
 
 
 def search_regex():
-    """Search the CSV file by regular expression"""
+    """Search CSV file contents by regular expression"""
     c_s()
     searching = True
-    while searching:
-        with open("worklog.csv", "r") as file:
-            file_reader = csv.reader(file)
-            reg_search = input(
-                """Please enter a regular expression to be searched:
+    while searching:        # Begin while loop
+        c_s()
+        reg_search = input(
+            """Please enter a regular expression to be searched:
 > """)
-            if reg_search:
-                p = re.compile(reg_search, re.IGNORECASE)
-                next(file_reader)
-                c_s()
-                holder = []
-                print("Search results for regex '{}':\n".format(reg_search))
-                for row in file_reader:
-                    date, task, time, notes = row
-                    for field in row:
-                        if re.search(p, field):
-                            holder.append(field)
-                            print(
-                                "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-                if len(holder) == 0:
-                    print("There were no results!")
-                search_again = input(
-                    """\nWould you like to do another search?
-
-Please press ENTER if YES, and N and ENTER if NO:
-> """).lower()
-                if search_again == 'n' or search_again == 'no':
-                    searching = False
-                    c_s()
-                    main()
-                else:
-                    c_s()
-                    searching = True
+        if reg_search:      # If there is reg search...
+            p = re.compile(reg_search, re.IGNORECASE)
+            c_s()
+            results = []
+            for row in reader():
+                date, task, time, notes = row
+                for field in row:
+                    if re.search(p, field):
+                        results.append(row)
+            if len(results):
+                printer(results, reg_search)
             else:
+                print("There were no results!")
+            if search_again() == False:
                 c_s()
-                input("Please enter a regular expression! Press ENTER to try again...")
-                c_s()
-
-    if search_again == 'n' or search_again == 'no':
-        searching = False
-        c_s()
-        main()
-    else:
-        c_s()
-        searching = True
+                main()
+            else:
+                searching = True
+        else:
+            c_s()
+            input("Please enter a regex! Press ENTER to continue...")
 
 
 def search_plain():
@@ -163,51 +146,36 @@ def search_plain():
     c_s()
     searching = True
     while searching:
-        with open("worklog.csv", "r") as file:
-            file_reader = csv.reader(file)
-            plain_search = input(
-                """Please enter a word or phrase to search:\n> """)
-            if plain_search:
-                c_s()
-                print("Search results for phrase '{}':\n".format(plain_search))
-                results = []
-                for row in file_reader:
-                    date, task, time, notes = row
-                    for field in row:
-                        if plain_search in field:
-                            results.append(plain_search)
-                            print(
-                                "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-                        elif plain_search.lower() in field:
-                            results.append(plain_search)
-                            print(
-                                "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-                        elif plain_search.upper() in field:
-                            results.append(plain_search)
-                            print(
-                                "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-                        elif plain_search.capitalize() in field:
-                            results.append(plain_search)
-                            print(
-                                "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-                if len(results) == 0:
-                    print("There were no results!")
-                search_again = input(
-                    """\nWould you like to do another search?
-
-Please press ENTER if YES, or N and ENTER if NO:
-> """).lower()
-                if search_again == 'n' or search_again == 'no':
-                    searching = False
-                    c_s()
-                    main()
-                else:
-                    c_s()
-                    searching = True
+        c_s()
+        plain_search = input(
+            """Please enter a word or phrase to search:\n> """)
+        if plain_search:
+            c_s()
+            results = []
+            for row in reader():
+                date, task, time, notes = row
+                for field in row:
+                    if plain_search in field:
+                        results.append(row)
+                    elif plain_search.lower() in field:
+                        results.append(row)
+                    elif plain_search.upper() in field:
+                        results.append(row)
+                    elif plain_search.capitalize() in field:
+                        results.append(row)
+            if len(results):
+                printer(results, plain_search)
             else:
+                print("There were no results!")
+            if search_again() == False:
                 c_s()
-                input("Please enter a plain text phrase! Press ENTER to start over...")
-                c_s()
+                main()
+            else:
+                searching = True
+        else:
+            c_s()
+            input("Please enter a plain text phrase! Press ENTER to start over...")
+            c_s()
 
 
 def search_date():
@@ -215,71 +183,50 @@ def search_date():
     c_s()
     searching = True
     while searching:
-        with open("worklog.csv", "r") as file:
-            file_reader = csv.reader(file)
-            date_search = input(
-                """Please enter a date to search in MM/DD/YYYY format:
+        c_s()
+        date_search = input(
+            """Please enter a date to search in MM/DD/YYYY format:
 > """)
-            if date_search and re.match(r'\d\d/\d\d/\d\d\d\d', date_search):
-                results = []
-                for row in file_reader:
-                    date, task, time, notes = row
-                    if date_search in date:
-                        results.append(date)
-                        print(
-                            "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-                if len(results) == 0:
-                    c_s()
-                    print("There were no results!")
-                search_again = input(
-                    """\nWould you like to do another search?
-
-Please press ENTER if YES, or N and ENTER if NO:
-> """).lower()
-                if search_again == 'n' or search_again == 'no':
-                    searching = False
-                    c_s()
-                    main()
-                else:
-                    c_s()
-                    searching = True
+        if date_search and re.match(r'\d\d/\d\d/\d\d\d\d', date_search):
+            results = []
+            for row in reader():
+                date, task, time, notes = row
+                if date_search in date:
+                    results.append(row)
+            if len(results):
+                printer(results, date_search)
             else:
-                c_s()
-                input("Please enter a date! Press ENTER to start over...")
-                c_s()
+                print("There were no results!")
+            if search_again() == False:
+                main()
+            else:
+                searching = True
+
+        else:
+            c_s()
+            input("Please enter a date! Press ENTER to start over...")
+            c_s()
 
 
 def search_minutes():
     """Search CSV file by the number of minutes a task took"""
     c_s()
     minute = verify_num()
-    with open("worklog.csv", "r") as file:
-        file_reader = csv.reader(file)
+    results = []
+    for row in reader():
+        date, task, time, notes = row
+        if row[2] == minute:
+            results.append(row)
+    if len(results):
+        printer(results, minute)
+    else:
         c_s()
-        print("Search results for time '{}' minutes:\n".format(minute))
-        holder = []
-        for row in file_reader:
-            date, task, time, notes = row
-            if row[2] == minute:
-                holder.append(row[2])
-                print(
-                    "-{} - {}, {} minutes: {}".format(date, task, time, notes))
-        if len(holder) == 0:
-            c_s()
-            print("There were no results!")
-        search_again = input(
-            """\nWould you like to do another search?
-
-Please press ENTER if YES, or N and ENTER if NO:
-> """).lower()
-        if search_again == 'n' or search_again == 'no':
-            searching = False
-            c_s()
-            main()
-        else:
-            c_s()
-            search_minutes()
-
+        print("There were no results!")
+    if search_again() == False:
+        c_s()
+        main()
+    else:
+        search_minutes()
 
 
 def show_all_entries():
@@ -287,13 +234,8 @@ def show_all_entries():
     c_s()
     print("Here are all your entries, beginning with the most recent:\n")
     print("The format is [DATE and TIME] - [TASK], [TIME] minutes: [NOTES]\n")
-    with open("worklog.csv", "r") as file:
-        file_reader = csv.reader(file)
-        next(file_reader)
-        lines = [x for x in file_reader]
-        for row in lines[::-1]:
-            date, name, time, notes = row
-            print("{} - {}, {} minutes: {}".format(date, name, time, notes))
+    for row in reader()[::-1]:
+        print(', '.join(row))
     input("\n\nPress ENTER to return to the main prompt...")
     main()
 
